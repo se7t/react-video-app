82b1c1b83ab204a8b3fcc51383c73b56771b0bef
se7t <se7t.contact@gmail.com>
2021-04-03 20:32:08 +0200

    Add basic form functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React from 'react'
+import { Button, Container, Form, FormGroup, FormText, Input, Label } from 'reactstrap'
+import { Controller, useForm } from "react-hook-form"
+
+
 function App() {
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
+      <Container>
+        <h1 className="text-center">React Video App</h1>
+        <Form onSubmit={handleSubmit(onSubmit)}>          
+          <FormGroup>
+            <Label for="videoURL">Video URL:</Label>
+            <Controller 
+            name="videoUrl"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <Input {...field} />}
+            />            
+            <FormText>Supported platforms: YouTube, Vimeo.</FormText>
+          </FormGroup>
+          <Button type="submit" color="primary">Submit</Button>
+        </Form>
+      </Container>
     </div>
   );
 }
