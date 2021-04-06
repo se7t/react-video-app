ff9aab0d829311f1dbd43b68ce3183c4589b7f4b
se7t <se7t.contact@gmail.com>
2021-04-06 18:32:11 +0200

    Add YouTube video and alert rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import { useState } from 'react'
-import { Button, Container, Form, FormGroup, FormText, Input, Label } from 'reactstrap'
+import { Alert, Button, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Container, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap'
 import { Controller, useForm } from "react-hook-form"
 import axios from "axios"
 
 function App() {
   const { control, handleSubmit } = useForm();
   const [videos, setVideos] = useState([]);
+  const [success, setSuccess] = useState({});
 
   const onSubmit = (data) => {
-    axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${data.videoUrl}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet`)
-    .then(function (response) {
-      // change to specific part of response
-      if (response.data.pageInfo.totalResults !== 0) {
-        setVideos([...videos, response]);
-      } else {
-        console.log("Requested URL video does not exist.");
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+   
+    if ( !(videos.some(video => video.id === data.videoUrl))) {
+      axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${data.videoUrl}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet`)
+      .then(function (response) {
+        // change to specific part of response
+        if (response.data.pageInfo.totalResults !== 0) {
+          setVideos([...videos, response.data.items[0]]);
+          setSuccess({
+            bootstrapColor: "success",
+            bootstrapMessage: "Video successfully added."
+          })
+        } else {
+          setSuccess({
+            bootstrapColor: "danger",
+            bootstrapMessage: "Requested video does not exist."
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        setSuccess({
+          bootstrapColor: "danger",
+          bootstrapMessage: "Failed to fetch video. Check console for more information."
+        })
+      })
+    } else {
+      setSuccess({
+        bootstrapColor: "info",
+        bootstrapMessage: "Video already exists."
+      });
+    }
+    
   }  
 
 // regex for yt video key /\/([^\/]+)\/?$/
@@ -43,6 +64,28 @@ function App() {
           </FormGroup>
           <Button type="submit" color="primary">Submit</Button>
         </Form>
+        {videos.length > 0 &&
+          <Alert className="mt-4" color={success.bootstrapColor}>{success.bootstrapMessage}</Alert>
+          }
+        <div>
+          <Row xs="1" sm="2" xl="3">
+            {videos.map(video => {
+              return (
+                <Col className="mt-4" key={video.id}>
+                  <Card>
+                  <CardImg top width="100%" src={video.snippet.thumbnails.medium.url} alt="Card image cap" />
+                    <CardBody>
+                      <CardTitle tag="h5">{video.snippet.title}</CardTitle>
+                      <CardSubtitle tag="h6" className="mb-2 text-muted">{video.snippet.channelTitle}</CardSubtitle>
+                      <CardText>{(video.snippet.description).replace(/^(.{11}[^\s]*).*/, "$1") }</CardText>
+                      <Button>Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              )
+            })}
+          </Row>
+        </div>
       </Container>
     </div>
   );
