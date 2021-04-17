ceb4560cbedee0ab41f329feb5a3210d1831db79
se7t <se7t.contact@gmail.com>
2021-04-17 22:13:28 +0200

    Add localStorage for storing videos

--- src/components/VideoContext.jsx
+++ src/components/VideoContext.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 export const VideoContext = createContext();
 
 export const VideoProvider = (props) => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState(JSON.parse(localStorage.getItem('videosStorage')));
+
+  useEffect(() => {
+    localStorage.setItem('videosStorage', JSON.stringify([...videos]));
+  });
 
   return (
     <VideoContext.Provider value={[videos, setVideos]}>
